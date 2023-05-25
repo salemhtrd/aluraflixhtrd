2af0c798b4b4d4fa1be869bbfa4c775e66e208d1
David House <salem.mhtrd@gmail.com>
2023-05-24 20:09:30 -0600

    primer envio listo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Error404 } from "./pages/Error404.jsx";
+import { Video } from "./pages/Video";
+import { Categoria } from "./pages/Categoria.jsx";
+import { Cabecera } from "./components/Cabecera";
+import { PieDePagina } from "./components/PieDePagina";
+
+import { ThemeProvider } from "styled-components";
+import { temaClaro } from "./components/UI/Temas";
+import { EstilosGlobales } from "./EstilosGlobales";
+import { EditarVideo } from "./pages/EditarVideo.jsx";
+import { EditarCategoria } from "./pages/EditarCategoria.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={temaClaro}>
+            <EstilosGlobales />
+            <Router>
+                <Cabecera />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/video" element={<Video />} />
+                    <Route path="/video/:id" element={<EditarVideo />} />
+                    <Route path="/categoria" element={<Categoria />} />
+                    <Route path="/categoria/:id" element={<EditarCategoria />} />
+                    <Route path="*" element={<Error404 />} />
+                </Routes>
+                <PieDePagina />
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
+
